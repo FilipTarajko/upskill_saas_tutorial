c041a2fe317b0b440c6ee0bb7f9c3a33179c65f1
Filip Tarajko <fftt271@gmail.com>
2021-01-06 02:07:26 +0000

    trying to fix mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -9,12 +9,3 @@ class ContactMailer < ActionMailer::Base
     end
 end
 
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'fierce-sea-37170.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
