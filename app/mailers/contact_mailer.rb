323984f0e3a18ac7912aeb7139b8d98b0c275d25
Filip Tarajko <fftt271@gmail.com>
2021-01-06 16:51:19 +0000

    Changed 'from' part of email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ActionMailer::Base
         @name = name
         @email = email
         @body = body
-        mail(from: email, subject: 'Contact Form Message')
+        mail(from: 'filiptarajko@gmail.com', subject: 'Message from Heroku Website')
     end
 end
 
